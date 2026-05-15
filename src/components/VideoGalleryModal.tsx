38d70b017bd5c59b343d01b05869a1c9b447e4d1
ingozhou <ingozhou66@users.noreply.github.com>
2026-05-15 11:44:00 +0800

    Revert "fix: 视频添加多个全屏阻止属性（playsInline + webkit-playsinline + controlsList）"
    This reverts commit aa6c594117bf43b0000295b17d8e8a045b29689b.

--- src/components/VideoGalleryModal.tsx
+++ src/components/VideoGalleryModal.tsx
@@ -189,9 +189,6 @@ export function VideoGalleryModal({
                       className="w-full h-full object-contain"
                       preload="metadata"
                       playsInline
-                      webkit-playsinline="true"
-                      x5-playsinline="true"
-                      controlsList="nofullscreen"
                       onClick={() => setIsPlaying(!isPlaying)}
                     />
 
